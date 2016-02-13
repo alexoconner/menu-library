1bb4343b4f5b6a273ed4230c0facbae6d02419e7
Alexander Skrabl <alexander@skrabl.de>
2016-02-13 20:38:08 +0000

    added menu trigger class and some more html/css for menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,11 +1,34 @@
 /**
  * Simple vanilla js to trigger our menus
  */
-document.addEventListener("DOMContentLoaded", function(event) {
 
-    var trigger = document.querySelector('.menu-trigger');
+/**
+ * @class: MenuTrigger
+ * defines our trigger and target and simply add/removes a class
+ */
+class MenuTrigger {
+    constructor(options) {
+        this.options = options;
 
-    trigger.onclick = function() {
-        console.log('clicked');
-    };
-});
+        this.trigger(this.options.trigger, this.options.target);
+    }
+
+    trigger(trigger, target) {
+        document.addEventListener("DOMContentLoaded", function(event) {
+
+            var menuTrigger = document.querySelector(trigger);
+
+            menuTrigger.onclick = function() {
+                console.log('target is ' + target);
+            };
+        });
+    }
+}
+
+export default MenuTrigger;
+
+/**
+ * only attaching this to the window so the class can be called inside our templates
+ * @type {MenuTrigger}
+ */
+window.MenuTrigger = MenuTrigger;
