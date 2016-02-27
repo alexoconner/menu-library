4ea64cc467a5038de7f1357699c5f624cb3c39d2
Alexander Skrabl <alexander@skrabl.de>
2016-02-27 18:39:04 +0000

    change position of line when hovering over nav items

--- src/scripts/horizontal-follow-me-menu.js
+++ src/scripts/horizontal-follow-me-menu.js
@@ -14,20 +14,34 @@ export default function horizontalFollowMeMenu(movingLine, menuItem, menuWrapper
         var menuItems = document.querySelectorAll(menuItem);
         var wrapper = document.querySelector(menuWrapper);
 
-        var startWidth = 0;
-        var startPosition = 0;
+        var lineWidth = 0;
+        var linePosition = 0;
 
         console.log(wrapper.offsetLeft);
 
         for (var i = 0; i < menuItems.length; i++) {
-            if (menuItems[i].classList.contains('current') || menuItems[i].classList.contains('active')) {
-                startWidth = parseInt( window.getComputedStyle( menuItems[i] ).width );
-                startPosition = parseInt( menuItems[i].offsetLeft );
+            let item = menuItems[i];
+
+            // define initial position for line/underline
+            if (item.classList.contains('current') || item.classList.contains('active')) {
+
+                lineWidth = parseInt( window.getComputedStyle( item ).width );
+                linePosition = parseInt( item.offsetLeft );
             }
+
+            // move line when hovering over nav items
+            item.addEventListener('mouseover', () => {
+                lineWidth = parseInt( window.getComputedStyle( item ).width );
+                linePosition = parseInt( item.offsetLeft );
+
+                line.style.width = lineWidth + 'px';
+                line.style.left = linePosition + 'px';
+            });
+
         }
 
-        line.style.width = startWidth + 'px';
-        line.style.left = startPosition + 'px';
+        line.style.width = lineWidth + 'px';
+        line.style.left = linePosition + 'px';
         line.classList.add('on');
 
     });
