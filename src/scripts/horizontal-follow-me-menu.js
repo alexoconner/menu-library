1d5959d7fdc93a837e8284fb328e7117a756ab00
Alexander Skrabl <alexander@skrabl.de>
2016-02-27 18:26:46 +0000

    start position for line for current page

--- src/scripts/horizontal-follow-me-menu.js
+++ src/scripts/horizontal-follow-me-menu.js
@@ -9,21 +9,26 @@ import 'classlist-polyfill';
 export default function horizontalFollowMeMenu(movingLine, menuItem, menuWrapper) {
 
     document.addEventListener("DOMContentLoaded", function(event) {
-        var movingLine = document.querySelector(movingLine);
+
+        var line = document.querySelector(movingLine);
         var menuItems = document.querySelectorAll(menuItem);
-        var menuWrapper = document.querySelector(menuWrapper);
+        var wrapper = document.querySelector(menuWrapper);
 
         var startWidth = 0;
         var startPosition = 0;
 
-
-        console.log(menuWrapper);
+        console.log(wrapper.offsetLeft);
 
         for (var i = 0; i < menuItems.length; i++) {
             if (menuItems[i].classList.contains('current') || menuItems[i].classList.contains('active')) {
                 startWidth = parseInt( window.getComputedStyle( menuItems[i] ).width );
-                startPosition = parseInt( window.getComputedStyle( menuItems[i] ).width )
+                startPosition = parseInt( menuItems[i].offsetLeft );
             }
         }
+
+        line.style.width = startWidth + 'px';
+        line.style.left = startPosition + 'px';
+        line.classList.add('on');
+
     });
 }
