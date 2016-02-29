873955e24946fec4b164ee7893df493530e05685
Alexander Skrabl <alexander@skrabl.de>
2016-02-29 00:50:24 +0000

    added common bundler and removed not needed stuff from package json

--- src/scripts/common/index.js
+++ src/scripts/common/index.js
@@ -2,7 +2,7 @@
  * Index js file
  */
 
-import './scripts/menu';
-import horizontalFollowMeMenu from './scripts/horizontal-follow-me-menu';
+import './menu';
+import horizontalFollowMeMenu from './scripts/horizontal-follow-me-menu/horizontal-follow-me-menu';
 
 horizontalFollowMeMenu('.horizontal-follow-me-menu__line', '.horizontal-follow-me-menu__main__item', '.horizontal-follow-me-menu');
